c532f71781c203a893bcf270e3881f27cd10e84b
Alejandro Peral Taboada <alejandroperaltaboada@gmail.com>
2018-05-03 18:54:50 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ListaComponent } from "./lista/lista.component";
+import { EmptyComponent } from "./empty/empty.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { PisoListaComponent } from "./piso-lista/piso-lista.component";
+import { PisoDetalleComponent } from "./piso-detalle/piso-detalle.component";
+import { PisoNewComponent } from "./piso-new/piso-new.component";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaComponent,
+    EmptyComponent,
+    PisoListaComponent,
+    PisoDetalleComponent,
+    PisoNewComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
